8ed0c9208a32cd47a04fa3d0c73f247585b74106
naphtalilyn <124272736+naphtalilyn@users.noreply.github.com>
2023-10-13 10:49:05 -0400

    update

--- components/userList/userList.jsx
+++ components/userList/userList.jsx
@@ -5,43 +5,84 @@ import {
   ListItem,
   ListItemText,
   Typography,
-} from '@mui/material';
-import './UserList.css';
+}
+  from '@material-ui/core';
+import './userList.css';
 
+import { Link } from 'react-router-dom';
+
+import fetchModel from '../../lib/fetchModelData';
+
+/**
+ * Define UserList
+ */
 class UserList extends React.Component {
   constructor(props) {
     super(props);
+
+    // Problem #1
+    let userListModel = window.cs142models.userListModel();
     this.state = {
-      users: [],
-    };
+      userListModel: userListModel
+    }
+
+    // Problem #2
+    this.state = {
+      error: null,
+      isLoaded: false,
+      userListModel: []
+    }
+
+    this.handleSuccess = this.handleSuccess.bind(this);
+    this.handleError = this.handleError.bind(this);
   }
 
+  getList() {
+    const { error, isLoaded, userListModel } = this.state;
+
+    if (error || !isLoaded) {
+      return;
+    }
+
+    console.log(userListModel);
+
+    var listContent = [];
+    let itemId = 0;
+
+    let divider = <Divider key={itemId - 1} />;
+    for (let user of userListModel) {
+      let text = user.first_name + " " + user.last_name;
+      let listItem = <ListItem className="listItem"> {text} </ListItem>;
+
+
+      let path = "/photos/:userId" + user._id;
+      listContent.push(<Link to={path} key={itemId}> {listItem} {divider} </Link>);
+      itemId += 1;
+    }
+    return <List component="nav"> {listContent} </List>
+  }
+  handleSuccess(value) {
+    this.setState({
+      isLoaded: true,
+      userListModel: value.data
+    });
+  }
+  handleError(error) {
+    this.setState({
+      isLoaded: true,
+      error: error
+    });
+  }
   componentDidMount() {
-    // Fetch user data from the model
-    const users = window.models.userListModel();
-    this.setState({ users });
+    let promise = fetchModel("/user/list");
+    promise.then(this.handleSuccess, this.handleError);
   }
-
   render() {
-    const { users } = this.state;
     return (
       <div>
-        <Typography variant="body1">
-          This is the user list, which takes up 3/12 of the window.
-        </Typography>
-        <List component="nav">
-          {users.map((user) => (
-            <div key={user._id}>
-              <ListItem>
-                <ListItemText primary={`${user.first_name} ${user.last_name}`} />
-              </ListItem>
-              <Divider />
-            </div>
-          ))}
-        </List>
-        <Typography variant="body1">
-          The model comes in from window.models.userListModel()
-        </Typography>
+
+        {this.getList()}
+
       </div>
     );
   }
