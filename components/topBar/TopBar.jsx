3e1b60c6107996e11e152c34912b5b15bfafa41f
carterphoenix <143209429+carterphoenix@users.noreply.github.com>
2023-11-13 10:09:51 -0500

    Update TopBar.jsx

--- components/topBar/TopBar.jsx
+++ components/topBar/TopBar.jsx
@@ -1,122 +1,147 @@
 import React from 'react';
 import {
-  AppBar, Toolbar, Typography
-} from '@material-ui/core';
+    AppBar, Toolbar, Typography, Button, Divider, Box, Alert, Snackbar
+} from '@mui/material';
 import './TopBar.css';
 import axios from 'axios';
+
 /**
- * Define TopBar
+ * Define TopBar, a React componment of project #5
  */
 class TopBar extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // Problem #2
-    this.state = {
-      currUrl: this.props.match.url,
-
-      versionIsLoaded: false,
-      userIsLoaded: false,
-
-      error: null,
-
-      version: null,
-      userModel: null
+    constructor(props) {
+        super(props);
+        this.state = {
+            app_info: undefined,
+            photo_upload_show: false,
+            photo_upload_error: false,
+            photo_upload_success: false
+        };
+        this.handleLogout = this.handleLogout.bind(this);
+        this.handleNewPhoto = this.handleNewPhoto.bind(this);
     }
-
-    this.handleSuccess = this.handleSuccess.bind(this);
-    this.handleError = this.handleError.bind(this);
-  }
-  handleSuccess(value) {
-    const isTestSchema = Object.keys(value.data).includes("__v");
-    if (isTestSchema) {
-      this.setState({
-        versionIsLoaded: true,
-        version: value.data.__v
-      });
-    } else {
-      this.setState({
-        userIsLoaded: true,
-        userModel: value.data
-      });
+    componentDidMount() {
+        this.handleAppInfoChange();
     }
-  }
-  handleError(error) {
-    this.setState({
-      error: error
-    });
-  }
-  componentDidMount() {
-    axios.get("/test/info/").then(this.handleSuccess, this.handleError);
-
-    let url = this.props.match.url;
-    let uIndex = url.search(/\/photos\//i);
-    let pIndex = url.search(/\/users\//i);
-    const atHomePage = (uIndex === -1 && pIndex === -1);
-
-    if (!atHomePage) {
-      const currId = url.substring(url.lastIndexOf("/") + 1);
-      axios.get("/user/" + currId).then(this.handleSuccess, this.handleError);
-    }
-  }
-  componentDidUpdate(prevProps) {
-    const prevUrl = prevProps.match.url;
-    const currUrl = this.props.match.url;
-
-    if (prevUrl !== currUrl) {
-      let uIndex = currUrl.search(/\/photos\//i);
-      let pIndex = currUrl.search(/\/users\//i);
-      const atHomePage = (uIndex === -1 && pIndex === -1);
-
-      if (!atHomePage) {
-        const currId = currUrl.substring(currUrl.lastIndexOf("/") + 1);
-        axios.get("/user/" + currId).then(this.handleSuccess, this.handleError);
-      } else {
-      }
-    }
-  }
-  render() {
-    let url = this.props.match.url;
-    let uIndex = url.search(/\/photos\//i);
-    let pIndex = url.search(/\/users\//i);
-    const atHomePage = (uIndex === -1 && pIndex === -1);
-
-    let context = "";
-    if (atHomePage) {
-      const isLoaded = this.state.versionIsLoaded;
-      const error = this.state.error;
-      if (error || !isLoaded) {
-        context = "Home Page";
-      } else {
-        context = "Home Page " + this.state.version;
-      }
-    } else {
-      const isLoaded = this.state.userIsLoaded;
-      const error = this.state.error;
-      if (error || !isLoaded) {
-      } else {
-        let userModel = this.state.userModel;
-        let name = userModel.first_name + " " + userModel.last_name;
-        context = (uIndex === -1) ? name : name + "'s Photos";
-      }
-    }
-
-    return (
-      <AppBar className="cs142-topbar-appBar" position="absolute">
-        <Toolbar className="cs142-topbar-flexbox">
-          <Typography variant="h5" color="inherit" className="cs142-topbar-flexitem name">
-            Reagan Kan&apos;s FB
-          </Typography>
-
-          <Typography variant="h5" color="inherit" className="cs142-topbar-flexitem context">
+    handleLogout = (user) => {
+        axios.post("/admin/logout")
+            .then((response) =>
             {
-              context
-            }
-          </Typography>
+                this.props.changeUser(undefined);
+            })
+            .catch( error => {
+                this.props.changeUser(undefined);
+                console.log(error);
+            });
+    }
+
+    handleNewPhoto = (e) => {
+        e.preventDefault();
+        if (this.uploadInput.files.length > 0) {
+            const domForm = new FormData();
+            domForm.append('uploadedphoto', this.uploadInput.files[0]);
+            axios.post("/photos/new", domForm)
+                .then((response) => {
+                    this.setState({
+                        photo_upload_show: true,
+                        photo_upload_error: false,
+                        photo_upload_success: true
+                    });
+                })
+                .catch(error => {
+                    this.setState({
+                        photo_upload_show: true,
+                        photo_upload_error: true,
+                        photo_upload_success: false
+                    });
+                    console.log(error);
+                });
+        }
+    }
+
+    handleClose = () => {
+        this.setState({
+            photo_upload_show: false,
+            photo_upload_error: false,
+            photo_upload_success: false
+        });
+    }
+
+    handleAppInfoChange(){
+        const app_info = this.state.app_info;
+        if (app_info === undefined){
+            axios.get("/test/info")
+                .then((response) =>
+                {
+                    this.setState({
+                        app_info: response.data
+                    });
+                });
+        }
+    }
+
+  render() {
+    return this.state.app_info ? (
+      <AppBar className="topbar-appBar" position="absolute">
+        <Toolbar>
+            <Typography variant="h5" component="div" sx={{ flexGrow: 0 }} color="inherit">
+                {
+                this.props.user ?
+                    (
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                width: 'fit-content',
+                                '& svg': {
+                                    m: 1.5,
+                                },
+                                '& hr': {
+                                    mx: 0.5,
+                                },
+                            }}
+                        >
+                            <span>{"Hi " + this.props.user.first_name}</span>
+                            <Divider orientation="vertical" flexItem/>
+                            <Button variant="contained" onClick={this.handleLogout}>Logout</Button>
+                            <Divider orientation="vertical" flexItem/>
+                            <Button
+                                component = "label"
+                                variant = "contained"
+                            >
+                                Add Photo
+                                <input
+                                    type="file"
+                                    accept = "image/*"
+                                    hidden
+                                    ref={(domFileRef) => { this.uploadInput = domFileRef; }}
+                                    onChange={this.handleNewPhoto}
+                                />
+                            </Button>
+                            <Snackbar anchorOrigin={{vertical: 'top', horizontal: 'left'}} open={this.state.photo_upload_show} autoHideDuration={6000} onClose={this.handleClose}>
+                                {
+                                    this.state.photo_upload_success ?
+                                        <Alert onClose={this.handleClose} severity="success" sx={{ width: '100%' }}>Photo Uploaded</Alert> :
+                                        this.state.photo_upload_error ?
+                                            <Alert onClose={this.handleClose} severity="error" sx={{ width: '100%' }}>Error Uploading Photo</Alert> :
+                                            <div/>
+                                }
+                            </Snackbar>
+                        </Box>
+                    )
+                :
+                    ("Please Login")
+                }
+            </Typography>
+            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }} color="inherit" align="center">{this.props.main_content}</Typography>
+            <Typography variant="h5" component="div" sx={{ flexGrow: 0 }} color="inherit">Version: {this.state.app_info.version}</Typography>
         </Toolbar>
       </AppBar>
+    ) : (
+        <div/>
     );
   }
 }
 
 export default TopBar;
+
