fe8e83214c74237a80442b1084b72c169fbb2b2f
carterphoenix <143209429+carterphoenix@users.noreply.github.com>
2023-11-13 10:12:10 -0500

    Update userList.jsx

--- components/userList/userList.jsx
+++ components/userList/userList.jsx
@@ -1,92 +1,75 @@
 import React from 'react';
 import {
-  Divider,
   List,
-  ListItem,
+  ListItemButton,
   ListItemText,
-  Typography,
 }
-  from '@material-ui/core';
+from '@mui/material';
 import './userList.css';
-
-import { Link } from 'react-router-dom';
-
 import axios from 'axios';
 
 /**
- * Define UserList
+ * Define UserList, a React component of project #5
  */
 class UserList extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // Problem #1
-    let userListModel = window.cs142models.userListModel();
-    this.state = {
-      userListModel: userListModel
+    constructor(props) {
+        super(props);
+        this.state = {
+                users: undefined,
+                user_id: undefined
+            };
     }
 
-    // Problem #2
-    this.state = {
-      error: null,
-      isLoaded: false,
-      userListModel: []
+    componentDidMount() {
+        this.handleUserListChange();
     }
 
-    this.handleSuccess = this.handleSuccess.bind(this);
-    this.handleError = this.handleError.bind(this);
-  }
-
-  getList() {
-    const { error, isLoaded, userListModel } = this.state;
-
-    if (error || !isLoaded) {
-      return;
+    componentDidUpdate() {
+        const new_user_id = this.props.match?.params.userId;
+        //console.log(new_user_id);
+        const current_user_id = this.state.user_id;
+        //console.log(current_user_id);
+        if (current_user_id  !== new_user_id){
+            this.handleUserChange(new_user_id);
+        }
     }
 
-    console.log(userListModel);
-
-    var listContent = [];
-    let itemId = 0;
-
-    let divider = <Divider key={itemId - 1} />;
-    for (let user of userListModel) {
-      let text = user.first_name + " " + user.last_name;
-      let listItem = <ListItem className="listItem"> {text} </ListItem>;
-
-
-      let path = "/photos/:userId" + user._id;
-      listContent.push(<Link to={path} key={itemId}> {listItem} {divider} </Link>);
-      itemId += 1;
+    handleUserChange(user_id){
+        this.setState({
+            user_id: user_id
+        });
     }
-    return <List component="nav"> {listContent} </List>
-  }
-  handleSuccess(value) {
-    this.setState({
-      isLoaded: true,
-      userListModel: value.data
-    });
-  }
-  handleError(error) {
-    this.setState({
-      isLoaded: true,
-      error: error
-    });
-  }
-  componentDidMount() {
-    let promise = axios.get("/user/list");
-    promise.then(this.handleSuccess, this.handleError);
-  }
+
+    handleUserListChange(){
+        axios.get("/user/list")
+            .then((response) =>
+            {
+                this.setState({
+                    users: response.data
+                });
+            });
+    }
+
   render() {
-    return (
-      <div>
-
-        {this.getList()}
-
-      </div>
+    return this.state.users ?(
+        <div>
+        <List component="nav">
+            {
+                this.state.users.map(user => (
+                <ListItemButton selected={this.state.user_id === user._id}
+                                key={user._id}
+                                divider={true}
+                                component="a" href={"#/users/" + user._id}>
+                    <ListItemText primary={user.first_name + " " + user.last_name} />
+                </ListItemButton>
+            ))
+            }
+        </List>
+        </div>
+    ) : (
+        <div/>
     );
   }
 }
 
 export default UserList;
-
