a475c3311a4ee257d7444716ff124ad91d92c951
carterphoenix <143209429+carterphoenix@users.noreply.github.com>
2023-12-13 13:59:10 -0500

    Update photoShare.jsx

--- photoShare.jsx
+++ photoShare.jsx
@@ -4,9 +4,10 @@ import {
   HashRouter, Route, Switch
 } from 'react-router-dom';
 import {
-  Grid, Paper
+  Grid, Typography, Paper
 } from '@mui/material';
 import './styles/main.css';
+import {Redirect} from "react-router";
 
 // import necessary components
 import TopBar from './components/topBar/TopBar';
@@ -15,30 +16,30 @@ import UserList from './components/userList/userList';
 import UserPhotos from './components/userPhotos/userPhotos';
 import LoginRegister from "./components/loginRegister/loginRegister";
 
-import {Redirect} from "react-router";
 
 class PhotoShare extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      main_content: undefined,
+      page_content: undefined,
       user: undefined
     };
-    this.changeMainContent = this.changeMainContent.bind(this);
+    this.changeTopbarContent = this.changeTopbarContent.bind(this);
     this.changeUser = this.changeUser.bind(this);
   }
 
-  userIsLoggedIn(){
+  changeTopbarContent = (page_content) => {
+    this.setState({ page_content: page_content });
+  };
+
+  isUserLoggedIn(){
     return this.state.user !== undefined;
   }
-  changeMainContent = (main_content) => {
-    this.setState({ main_content: main_content });
-  };
 
   changeUser = (user) => {
     this.setState({user: user});
-    if (user === undefined) this.changeMainContent(undefined);
-  }
+    if (user === undefined) this.changeTopbarContent(undefined);
+  };
 
   render() {
     return (
@@ -46,44 +47,72 @@ class PhotoShare extends React.Component {
       <div>
       <Grid container spacing={8}>
         <Grid item xs={12}>
-          <TopBar main_content={this.state.main_content} user={this.state.user} changeUser={this.changeUser}/>
+          <TopBar page_content={this.state.page_content} user={this.state.user} changeUser={this.changeUser} />
         </Grid>
         <div className="main-topbar-buffer"/>
         <Grid item sm={3}>
           <Paper className="main-grid-item">
             {
-              this.userIsLoggedIn() ? <UserList/> : <div></div>
+              this.isUserLoggedIn() ? <UserList/> : <div></div>
             }
           </Paper>
         </Grid>
         <Grid item sm={9}>
           <Paper className="main-grid-item">
             <Switch>
+              {this.isUserLoggedIn() ?
+                (
+                <Route path="/users/:userId"
+                  render={ props => <UserDetail {...props} changeTopbarContent={this.changeTopbarContent} /> }
+                />
+                )
+              :
+                <Redirect path="/users/:userId" to="/login-register" />}
               {
-                this.userIsLoggedIn() ?
-                    <Route path="/users/:userId" render={ props => <UserDetail {...props} changeMainContent={this.changeMainContent}/> }/>
-                    :
-                    <Redirect path="/users/:userId" to="/login-register" />
-              }
-              {
-                this.userIsLoggedIn() ?
-                    <Route path="/photos/:userId" render ={ props => <UserPhotos {...props} changeMainContent={this.changeMainContent}/> }/>
-                    :
+              this.isUserLoggedIn() ?
+              (
+              <Route path="/photos/:userId"
+                render ={ props => <UserPhotos {...props} changeTopbarContent={this.changeTopbarContent} /> }
+              />
+              )
+              :
                     <Redirect path="/photos/:userId" to="/login-register" />
               }
               {
-                this.userIsLoggedIn() ?
-                    <Route path="/" render={() => (<div/>)}/>
+                this.isUserLoggedIn() ?
+                    (
+                        <Route path="/photos/:userId/:photoId"
+                               render ={ props => <UserPhotos {...props} changeMainContent={this.changeMainContent} /> }
+                        />
+                    )
                     :
-                    <Route path="/login-register" render ={ props => <LoginRegister {...props} changeUser={this.changeUser}/> } />
-              }
-               {
-                this.userIsLoggedIn() ?
-                    <Route path="/" render={() => (<div/>)}/>
-                    :
-                    <Route path="/" render ={ props => <LoginRegister {...props} changeUser={this.changeUser}/> } />
+                    <Redirect path="/photos/:userId" to="/login-register" />
               }
 
+              {this.isUserLoggedIn() ?
+              (
+              <Route exact path="/"
+                  render={() => (
+                  <Typography variant="body1">
+                    Welcome to CodeCrafters photosharing app!
+                  </Typography>
+                )}
+              />
+              )
+              :<Route path="/login-register" render ={ props => <LoginRegister {...props} changeUser={this.changeUser}/> } />}
+              {this.isUserLoggedIn() ?
+              (
+                <Route exact path="/"
+                render={() => (
+                <Typography variant="body1">
+                  Welcome to CodeCrafters photosharing app!
+                </Typography>
+                )}
+                />
+              )
+                :
+                <Route path="/" render ={ props => <LoginRegister {...props} changeUser={this.changeUser}/> } />}
+              {/* <Route path="/users" component={UserList}  /> */}
             </Switch>
           </Paper>
         </Grid>
@@ -94,7 +123,8 @@ class PhotoShare extends React.Component {
   }
 }
 
+
 ReactDOM.render(
-  <PhotoShare/>,
-  document.getElementById('photoshareapp')
+  <PhotoShare />,
+  document.getElementById('photoshareapp'),
 );
